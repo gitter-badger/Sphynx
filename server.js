2c5ef13d9a780060b7491595db8a98f5e3ebcdd7
Adam Kelly <adam.guitar12@yahoo.ie>
2016-07-01 23:14:13 +0000

    Add SCSS Middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const dotenv = require('dotenv');
 const compression = require('compression');
 const path = require('path');
+const sass = require('node-sass-middleware');
 
 // Load environment variables from .env file
 dotenv.load();
@@ -15,6 +16,14 @@ app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
 app.use(compression());
 app.use(logger('dev'));
+app.use(
+    sass({
+        src: __dirname + '/sass',
+        dest: __dirname + '/public/css',
+        prefix: '/css',
+        debug: true,
+    })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 require('./router/main.js')(app);
 
